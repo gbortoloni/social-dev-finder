28c01f63b4d3ac4d1f8b7413c7606833901202aa
Giuliano Bortoloni <giulianoagria@hotmail.com>
2020-01-15 21:47:00 -0300

    feat(frontend): Add remove dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import api from "./services/api";
 import DevItem from "./components/DevItem/index.jsx";
 import DevForm from "./components/DevForm/index.jsx";
 
+import "font-awesome/css/font-awesome.min.css";
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
@@ -11,6 +12,7 @@ import "./Main.css";
 
 function App() {
   const [devs, setDevs] = useState([]);
+  const [removeDevs, setRemoveDevs] = useState("");
 
   useEffect(() => {
     async function loadDevs() {
@@ -20,10 +22,24 @@ function App() {
     loadDevs();
   }, []);
 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, [removeDevs]);
+
   async function handleAddDev(data) {
     const response = await api.post("/devs", data);
 
     setDevs([...devs, response.data]);
+  }
+
+  async function handleRemoveDev(id) {
+    setRemoveDevs(id);
+    await api.delete(`/devs/${id}`);
+    setRemoveDevs("");
   }
 
   return (
@@ -35,7 +51,7 @@ function App() {
       <main>
         <ul>
           {devs.map(dev => (
-            <DevItem key={dev._id} dev={dev} />
+            <DevItem key={dev._id} dev={dev} remove={handleRemoveDev} />
           ))}
         </ul>
       </main>
--- frontend/src/components/DevItem/index.jsx
+++ frontend/src/components/DevItem/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
-import "./styles.css"
+import "./styles.css";
 
-function DevItem({dev}) {
+function DevItem({ dev, remove }) {
+  async function handleRemove(e) {
+    e.preventDefault();
+    await remove(dev._id);
+  }
+
   return (
     <li className="dev-item">
+      <button className="remove" onClick={handleRemove}>
+        <i className="fa fa-trash-o"></i>
+      </button>
+      <button className="edit">
+        <i className="fa fa-edit"></i>
+      </button>
       <header>
         <img src={dev.avatar_url} alt={dev.name} />
         <div className="user-info">
